286919fda1287fa99cd457b368bd3d15b767d13b
Mark Delgado <markdelgado94@gmail.com>
2021-07-23 23:25:48 -0700

    created loader and message component

--- frontend/src/screens/MainScreen/MainScreen.js
+++ frontend/src/screens/MainScreen/MainScreen.js
@@ -1,15 +1,55 @@
-import React from "react"
-// import "./MainScreen.css"
-import GalleryScreen from "../GalleryScreen/GalleryScreen"
+// import React from "react"
+// // import "./MainScreen.css"
+// import GalleryScreen from "../GalleryScreen/GalleryScreen"
 
-const MainScreen = () => {
+// const MainScreen = () => {
+//   return (
+//     <div>
+//       <div class='main'>
+//         <GalleryScreen />
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default MainScreen
+
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import Message from "../components/Message"
+import Loader from "../components/Loader"
+import Product from "../components/Product"
+import { Row, Col } from "react-bootstrap"
+import { listProducts } from "../actions/productActions"
+
+const HomeScreen = () => {
+  const dispatch = useDispatch()
+
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
+
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
+
   return (
-    <div>
-      <div class='main'>
-        <GalleryScreen />
-      </div>
-    </div>
+    <>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
   )
 }
 
-export default MainScreen
+export default HomeScreen
